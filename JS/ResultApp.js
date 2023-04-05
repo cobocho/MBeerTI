fa442e0468b1539d235a0da4b66e4cd99df2f8e2
Cobocho <99083803+Cobocho@users.noreply.github.com>
2023-04-06 00:27:17 +0900

    feat: 결과 페이지 데이터 변경 로직 구현

---
+++ JS/ResultApp.js
@@ -0,0 +1,61 @@
+import Bubble from "./components/Bubble.js";
+import BeerList from "./constants/BeerList.js";
+import { $ } from "./Utils.js";
+
+class App {
+  #result = null;
+  constructor() {
+    this.init();
+  }
+  init() {
+    const urlParams = new URL(location.href).searchParams;
+    const name = urlParams.get("id");
+    this.#result = BeerList.find(({ id }) => id === name);
+    this.addBubbles(50);
+    console.log(this.#result);
+    this.changeBeerData();
+    this.addEvent();
+  }
+  addBubbles(amount) {
+    for (let i = 0; i < amount; i++) {
+      new Bubble($(".bubbles"), "span", null, "bubble");
+    }
+  }
+  changeBeerData() {
+    this.changeBeerName();
+    this.changeBeerDesc();
+    this.changeFoodList();
+    this.changeColor();
+    this.changeStatus();
+  }
+  changeBeerName() {
+    $(".beer-name-kor").innerHTML = this.#result.name;
+    $(".beer-name-eng").innerHTML = this.#result.nameEng;
+  }
+  changeBeerDesc() {
+    $(".beer-desc").innerHTML = this.#result.desc;
+  }
+  changeFoodList() {
+    $(".food-list").innerHTML = `
+      ${this.#result.foods.map((food) => `<li>${food}</li>`).join("")}
+    `;
+  }
+  changeStatus() {
+    $(".ibu .status-desc").innerHTML = this.#result.ibu;
+    $(".alc .status-desc").innerHTML = this.#result.alc;
+    $(".malty .status-bar-inner").style.width = this.#result.malty + "%";
+    $(".hoppy .status-bar-inner").style.width = this.#result.hoppy + "%";
+  }
+  changeColor() {
+    $("body").style.backgroundColor = this.#result.color;
+    $(".beer-graphic").style.backgroundColor = this.#result.color;
+  }
+  addEvent() {
+    $(".other-result");
+    $(".retry").addEventListener("click", () => {
+      location.href = `/`;
+    });
+  }
+}
+
+const app = new App();
